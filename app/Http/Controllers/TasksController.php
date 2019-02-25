071ab9a04d37180263f2f62ea30b668b4de63b3e
Nozomi_Saito <saitonozomi22@gmail.com>
2019-02-25 23:29:43 +0900

    redirect

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -64,7 +64,10 @@ class TasksController extends Controller
     public function show($id)
     {
         $task = Task::find($id);
-        return view('tasks.show',['task' => $task,]);
+        if (\Auth::id() === $task->user_id) {
+            return view('tasks.show',['task' => $task,]);
+        }
+        return redirect('/');
     }
 
     /**
@@ -76,10 +79,12 @@ class TasksController extends Controller
     public function edit($id)
     {
         $task = Task::find($id);
-
-        return view('tasks.edit', [
-            'task' => $task,
-        ]);
+        if (\Auth::id() === $task->user_id) {
+            return view('tasks.edit', [
+                 'task' => $task,
+            ]);
+        }
+        return redirect('/');
     }
 
     /**
